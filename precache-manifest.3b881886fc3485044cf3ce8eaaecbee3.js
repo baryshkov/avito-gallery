d801de6e28755e35592609bab76b221f7e3908a9
baryshkov <ambaryshkov@gmail.com>
2020-01-12 18:01:17 +0300

    Updates

---
+++ precache-manifest.3b881886fc3485044cf3ce8eaaecbee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407ec5e274c815472ad3dc7a566580a5",
+    "url": "/avito-gallery/index.html"
+  },
+  {
+    "revision": "0cedc0dc93e479e9a2ab",
+    "url": "/avito-gallery/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cf01e51994845fa101b0",
+    "url": "/avito-gallery/static/css/main.7fbccf8d.chunk.css"
+  },
+  {
+    "revision": "0cedc0dc93e479e9a2ab",
+    "url": "/avito-gallery/static/js/2.b1c2404b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/avito-gallery/static/js/2.b1c2404b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf01e51994845fa101b0",
+    "url": "/avito-gallery/static/js/main.5feeca85.chunk.js"
+  },
+  {
+    "revision": "8320b87eb9260f914dee",
+    "url": "/avito-gallery/static/js/runtime-main.b2861c51.js"
+  }
+]);
